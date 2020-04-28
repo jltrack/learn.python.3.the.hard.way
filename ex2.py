2375cf20056ee1e92c4310f45f3d083a7830d429
John Track <54856993+jltrack@users.noreply.github.com>
2020-04-27 23:27:19 -0400

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python on this line.
+print("I could have # code like this.")
+# The hash above is inside the strong so the code still prints, it's a character, not a comment
+print("This will run.")
